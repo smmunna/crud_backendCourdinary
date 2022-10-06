564e4d592efd754421512db8c74187f2163bb8a8
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-06 14:00:48 -0700

    update user.js
    Completed Backend with Node.js functionality for CRUD...

--- routes/user.js
+++ routes/user.js
@@ -49,17 +49,32 @@ router.delete('/:id',async(req,res)=>{
     }
 })
 
+// Find Individual user by Id;
+router.get('/:id',async(req,res)=>{
+    try {
+        let user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (error) {
+        res.send('User not Found individually');
+    }
+})
+
 // Updating the User name , Photo
 router.put('/:id', upload.single("image"), async(req,res)=>{
     try {
         let user = await User.findById(req.params.id);
         await cloudinary.uploader.destroy(user.cloudinary_id);
 
-        const result = await cloudinary.uploader.upload(req.file.path);
+        // Upload image to cloudinary
+        let result
+        if(req.file){
+            result = await cloudinary.uploader.upload(req.file.path);
+        }
+       
         const data = {
             name: req.body.name || user.name,
-            avatar: result.secure_url || user.avatar,
-            cloudinary_id: result.public_id || user.cloudinary_id,
+            avatar: result?.secure_url || user.avatar,
+            cloudinary_id: result?.public_id || user.cloudinary_id,
         };
         user = await User.findByIdAndUpdate(req.params.id, data,{new: true})
         res.json(user);
